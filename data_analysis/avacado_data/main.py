dceca3d7d3563d4c7e3ca2e5596b55868f7752cc
Jeff Lin <jefferson20071001@gmail.com>
2025-01-19 14:11:18 -0500

    fun

---
+++ data_analysis/avacado_data/main.py
@@ -0,0 +1,21 @@
+# some cool functions 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+
+df = pd.read_csvdf = pd.read_csv(r"data_analysis\avacado_data\avacado_dataset\avocado.csv")
+
+
+def multiple_categorical_graph(category_index)
+    for region in df['region'].unique()[:16]:
+        print(region)
+        region_df = df.copy()[df['region']==region]
+        region_df.set_index('Date', inplace=True)
+        region_df.sort_index(inplace=True)
+        region_df[f"{region}_price25ma"] = region_df["AveragePrice"].rolling(25).mean()
+
+        if graph_df.empty:
+            graph_df = region_df[[f"{region}_price25ma"]]  # note the double square brackets!
+        else:
+            graph_df = graph_df.join(region_df[f"{region}_price25ma"])
+    graph_df.plot(figsize=(8,5), legend=False)
