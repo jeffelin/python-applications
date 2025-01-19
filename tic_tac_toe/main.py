d2b0d38fa6d41c184701b8ea499b8a6b896301bf
Jeff Lin <jefferson20071001@gmail.com>
2025-01-18 20:57:32 -0500

    Update main.py

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,4 +1,4 @@
-# function building 
+# functions
 
 def addition(x,y): 
     return x+y 
@@ -9,21 +9,9 @@ def game_board_v1():
     game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
-
-    
-    # just putting the game board of the game 
-
-    print("   0  1  2") # status 
-     
+    print("   0  1  2")
     for count, row in enumerate(game):
         print(count, row)
-
-    # game[0][1] = 2 # now we are assigning
-    
-    # # what if we did a random sequence of numbers into the list arrays 
-    # print("   0  1  2") # status 
-    # for count, row in enumerate(game):
-    #     print(count, row)
 
 game = [[0, 0, 0],
         [0, 0, 0],
@@ -35,28 +23,43 @@ def game_board_v2(current_game, player=0, row=0, column=0, just_display=False):
     if not just_display:
         game[row][column] = player
     for count, row in enumerate(game): 
-        print(count, row) # loops through normal game to print game and then new game
-    # print(game) 
+        print(count, row) 
     return current_game
 
 
 
 def check_winner_of_game(current_game_board): 
    
-    for row in game: # game will be initialized 
-        print(row)# checker
+    for row in game:
+        print(row)
         if row.count(row[0]) == len(row) and row[0] != 0: 
             print(f"Player {row[0]} is the Winner")
     
     for column in range(len(game[0])):
         checker = [] 
-        for row in game: # game will be initialized 
+        for row in game:
             checker.append(row[column])
             if checker.count(row[0]) == len(row) and row[0] != 0: 
                 print(f"Player {row[0]} is the Winner")
 
-    # print("   0  1  2")
+
+
+
+
+
+# ----------------------------------------------------------------------
+
+
+# # dump:
+#     # print("   0  1  2")
     # if not just_display:
     #     game[row][column] = player
+    # for count, row in enumerate(game):
+    #     print(count, row)
+
+    # game[0][1] = 2 # now we are assigning
+    
+    # # what if we did a random sequence of numbers into the list arrays 
+    # print("   0  1  2") # status 
     # for count, row in enumerate(game):
     #     print(count, row)
