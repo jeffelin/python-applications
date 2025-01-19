6e5bcf4513b62d62ebb7dbe86aff012b2b0285d5
Jeff Lin <jefferson20071001@gmail.com>
2025-01-19 16:42:50 -0500

    Update main.py

--- data_analysis/avacado_data/main.py
+++ data_analysis/avacado_data/main.py
@@ -3,8 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 
-df = pd.read_csvdf = pd.read_csv(r"data_analysis\avacado_data\avacado_dataset\avocado.csv")
+df = pd.read_csvdf = pd.read_cs(r"data_analysis\avacado_data\avacado_dataset\avocado.csv")
 
+
+def pandas_dataframes():
+    n = 0 
+    df.head(n) 
+    df.tail(n) 
+    df_COLUMN = df[df['COLUMN'] == 'ROW_VALUES']
+    df_NEW_INDEX = df.set_index('NEW_INDEX')
 
 def multiple_categorical_graph(category_index)
     for region in df['region'].unique()[:16]:
