d5d91d99a9957bdadd2108eca526a8116f068a6e
Jeff Lin <jefferson20071001@gmail.com>
2025-01-20 08:42:56 -0500

    some playground stuff

--- data_analysis/minimum_wage/main.py
+++ data_analysis/minimum_wage/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # functions
 
+df = pd.read_csv("python-applications\data_analysis\minimum_wage\minimum_wage_1968-2020\MinimumWageData.csv")
 
 def states_to_columns(desired_data_points):
     act_min_wage = pd.DataFrame()
@@ -14,3 +15,12 @@ def states_to_columns(desired_data_points):
         else:
             act_min_wage = act_min_wage.join(group.set_index("Year")[[desired_data_points]].rename(columns={desired_data_points:name}))
     return act_min_wage.head()
+
+def pandas_dataframes():
+    n = 0 
+    df.head(n) 
+    df.tail(n) 
+    df_COLUMN = df[df['COLUMN'] == 'ROW_VALUES']
+    df_NEW_INDEX = df.set_index('NEW_INDEX')
+    df.cov().head()
+    df.corr().head()
